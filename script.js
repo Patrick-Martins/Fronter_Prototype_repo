c174b33598cb2297f0ca93bf75c3a516b8386d7a
Mac <mac@Macs-MacBook-Air-2.local>
2019-05-29 09:36:09 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+/*var mainBodyTop =
+document.getElementById("mainBody").style.getPropertyValue("margin-top");
+
+/*if (headHeight!=mainBodyTop){
+    document.getElementById("mainBody").style.marginTop= headHeight;
+}
+console.log(document.getElementById("mainBody").setAttribute('style', margin-top:document.querySelector("header").style.getPropertyValue("height"));
+*/
