e794bd11e75413c98af5496c6c77f8fe8f225661
Mac <mac@Macs-MacBook-Air-2.local>
2019-05-31 11:53:17 +0200

    added login page and improvements

--- mainPage.js
+++ mainPage.js
@@ -1,5 +1,20 @@
 const drone = document.getElementById("newsDrone");
+const content = document.getElementById("pinned3");
+const analytics = document.getElementById("pinned2");
+const submissions = document.getElementById("pinned1");
 
 drone.addEventListener('click', function (){
     window.location.href = 'drone.html';
 });
+
+content.addEventListener('click', function (){
+    window.location.href = 'drone.html';
+});
+analytics.addEventListener('click', function (){
+    window.location.href = 'drone.html';
+});
+submissions.addEventListener('click', function (){
+    window.location.href = 'drone.html';
+});
+
+
---
+++ messages.js
@@ -0,0 +1,5 @@
+const message1 = document.getElementById("Messages_1");
+
+message1.addEventListener('click', function (){
+    window.location.href = 'message1.html';
+});
